76ac39f14789f232b75da39fe57fe2716b739109
teklu abayneh <tekluabayneh@gmail.com>
2025-03-11 19:09:43 +0300

    Fix rendering in Quotes page

--- kukanApi/client/src/pages/Quotes/Quotes.jsx
+++ kukanApi/client/src/pages/Quotes/Quotes.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 import { gsap } from "gsap";
 
 // Assets in public/assets/
-import openIcon from "/assets/open.svg";
-import closeIcon from "/assets/close.svg";
+import openIcon from "../../assets/open.svg";
+import closeIcon from "../../assets/close.svg";
 
 const QuotesDocs = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
@@ -56,7 +56,7 @@ const QuotesDocs = () => {
               <Link to="/">Home</Link>
             </li>
             <li className="capitalize font-bold cursor-pointer hover:underline transition-all">
-              <Link to="/docs/users">Doc</Link>
+              <Link to="/users">Doc</Link>
             </li>
             <li className="capitalize font-bold cursor-pointer hover:underline transition-all">
               <Link to="/api">ApiKey</Link>
@@ -102,7 +102,7 @@ const QuotesDocs = () => {
             </li>
             <li className="capitalize font-bold cursor-pointer hover:underline transition-all">
               <Link
-                to="/docs/users"
+                to="/users"
                 className="text-[2.5rem] font-extrabold text-white customfont"
                 onClick={toggleNav}
               >
@@ -339,7 +339,7 @@ const QuotesDocs = () => {
           <div className="sticky top-96 text-center">
             <ul className="flex flex-col gap-24 items-center">
               <li className="flex items-center group transition-all relative">
-                <Link to="/docs/users">
+                <Link to="/users">
                   <span className="absolute hidden top-1 right-9 group-hover:block bg-black rounded px-2">
                     users!
                   </span>
@@ -362,7 +362,7 @@ const QuotesDocs = () => {
                 </Link>
               </li>
               <li className="flex items-center group transition-all relative">
-                <Link to="/docs/product">
+                <Link to="/product">
                   <span className="absolute hidden top-1 right-9 group-hover:block bg-black rounded px-2">
                     product!
                   </span>
@@ -385,7 +385,7 @@ const QuotesDocs = () => {
                 </Link>
               </li>
               <li className="flex items-center group transition-all relative">
-                <Link to="/docs/posts">
+                <Link to="/posts">
                   <span className="absolute hidden top-1 right-9 group-hover:block bg-black rounded px-2">
                     posts!
                   </span>
@@ -408,7 +408,7 @@ const QuotesDocs = () => {
                 </Link>
               </li>
               <li className="flex items-center group transition-all relative">
-                <Link to="/docs/quotes">
+                <Link to="/quotes">
                   <span className="absolute hidden top-1 right-9 group-hover:block bg-black rounded px-2">
                     quotes!
                   </span>
