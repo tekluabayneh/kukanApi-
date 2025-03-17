50b9338c19b59cc02fe107de4003470d3340da10
teklu abayneh <tekluabayneh@gmail.com>
2025-03-17 18:10:51 +0300

    Add Loading.jsx with Tailwind-centered container for loader UI

---
+++ kukanApi/client/src/components/Loading/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Loading.css";
+const Loading = () => {
+  return (
+    <div className="w-full h-screen grid bg-black place-items-center">
+      <div className="loader "></div>.
+    </div>
+  );
+};
+
+export default Loading;
