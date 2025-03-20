fa53f5a4de35fdd65748458e3e4ba9371e398cae
teklu abayneh <tekluabayneh@gmail.com>
2025-03-20 15:29:11 +0300

    Update Auth.jsx to integrate refactored Login component with form handling

--- kukanApi/client/src/components/Auth.jsx
+++ kukanApi/client/src/components/Auth.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 import { gsap } from "gsap";
+import Login from "../pages/Auth/Login";
+import Register from "../pages/Auth/Register";
 
 const Auth = () => {
   const {
@@ -60,183 +62,29 @@ const Auth = () => {
   return (
     <div className="bg-gradient-to-r from-blue-500 to-teal-500 h-screen flex gap-5 items-center justify-center relative">
       {/* Login Form */}
-      <div
+      <Login
         ref={loginRef}
-        className={`login bg-white p-8 rounded-lg shadow-xl max-w-sm w-full ${
-          !isLogin ? "hidden" : ""
-        }`}
-      >
-        <h2 className="text-3xl font-semibold text-center text-gray-800 mb-6">
-          Login
-        </h2>
-        <p className="show_loginmessage text-red-500">{loginMessage}</p>
-        <form className="space-y-6" onSubmit={handleSubmit(handleLoginSubmit)}>
-          {/* Email Input */}
-          <div className="space-y-2">
-            <label
-              htmlFor="login_email"
-              className="text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              {...register("login_email", {
-                required: "email is mandatory",
-              })}
-              placeholder="Enter your email"
-              className="login_email w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <span className="text-red-500 pt-1">
-              {errors.login_email?.message}
-            </span>
-          </div>
-
-          {/* Password Input */}
-          <div className="space-y-2">
-            <label
-              htmlFor="login_password"
-              className="text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              {...register("login_password", {
-                required: "password is mandatory",
-              })}
-              placeholder="Enter your password"
-              className="login_password w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-            />
-            <span className="text-red-500 pt-1">
-              {errors.login_password?.message}
-            </span>
-          </div>
-
-          {/* Forgot Password Link */}
-          <div className="flex justify-between items-center">
-            <Link to="#" className="text-sm text-blue-500 hover:text-blue-700">
-              Forgot password?
-            </Link>
-          </div>
-
-          {/* Submit Button */}
-          <div>
-            <button
-              id="loginBtn"
-              type="submit"
-              className="w-full bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700 transition-all focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              Login
-            </button>
-          </div>
-        </form>
-
-        <div className="mt-4 text-center">
-          <p className="text-sm text-gray-600">
-            Don't have an account?{" "}
-            <button
-              onClick={toggleForm}
-              className="loginBtn text-blue-500 hover:text-blue-700"
-            >
-              Sign up
-            </button>
-          </p>
-        </div>
-      </div>
+        isLogin={isLogin}
+        toggleForm={toggleForm}
+        loginMessage={loginMessage}
+        handleSubmit={handleSubmit}
+        handleLoginSubmit={handleLoginSubmit}
+        register={register}
+        errors={errors}
+      />
 
       {/* Register Form */}
-      <div
+
+      <Register
         ref={registerRef}
-        className={`register bg-white p-8 rounded-lg shadow-xl max-w-sm w-full ${
-          isLogin ? "hidden" : ""
-        }`}
-      >
-        <h2 className="text-3xl font-semibold text-center text-gray-800 mb-6">
-          Register
-        </h2>
-        <p className="show_registermessage text-red-500">{registerMessage}</p>
-        <form
-          className="space-y-6"
-          onSubmit={handleSubmit(handleRegisterSubmit)}
-        >
-          {/* Email Input */}
-          <div className="space-y-2">
-            <label
-              htmlFor="register_email"
-              className="text-sm font-medium text-gray-700"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              name="email"
-              {...register("register_email", {
-                required: "email is mandatory",
-              })}
-              className="register_email w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter your email"
-            />
-            <span className="text-red-500 pt-1">
-              {errors.register_email?.message}
-            </span>
-          </div>
-
-          {/* Password Input */}
-          <div className="space-y-2">
-            <label
-              htmlFor="register_password"
-              className="text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              {...register("register_password", {
-                required: "password is mandatory",
-              })}
-              className="register_password w-full p-3 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter your password"
-            />
-            <span className="text-red-500 pt-1">
-              {errors.register_password?.message}
-            </span>
-          </div>
-
-          {/* Forgot Password Link */}
-          <div className="flex justify-between items-center">
-            <Link to="#" className="text-sm text-blue-500 hover:text-blue-700">
-              Forgot password?
-            </Link>
-          </div>
-
-          {/* Submit Button */}
-          <div>
-            <button
-              id="registerBtn"
-              type="submit"
-              className="w-full bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700 transition-all focus:outline-none focus:ring-2 focus:ring-blue-500"
-            >
-              Register
-            </button>
-          </div>
-        </form>
-
-        <div className="mt-4 text-center">
-          <p className="text-sm text-gray-600">
-            Already have an account?{" "}
-            <button
-              onClick={toggleForm}
-              className="registerBtn text-blue-500 hover:text-blue-700"
-            >
-              Login
-            </button>
-          </p>
-        </div>
-      </div>
+        isLogin={isLogin}
+        toggleForm={toggleForm}
+        registerMessage={registerMessage}
+        handleSubmit={handleSubmit}
+        handleRegisterSubmit={handleRegisterSubmit}
+        register={register}
+        errors={errors}
+      />
     </div>
   );
 };
