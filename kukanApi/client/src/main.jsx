41b21e140ffd024b4d6c2f244be38504c9cd6cc8
teklu abayneh <tekluabayneh@gmail.com>
2025-03-21 19:53:53 +0300

    Configure QueryClientProvider in main.jsx for React Query setup

--- kukanApi/client/src/main.jsx
+++ kukanApi/client/src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Prism from 'prismjs';
-import 'prismjs/themes/prism-tomorrow.css';
-
-// Prism will auto-highlight code blocks on page load
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+let queryClient = new QueryClient();
+//  Prism will auto-highlight code blocks on page load
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>
 );
