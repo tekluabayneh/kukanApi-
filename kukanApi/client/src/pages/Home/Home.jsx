01489cdb7b41879f44b51f307737fe77143f1f86
teklu abayneh <tekluabayneh@gmail.com>
2025-03-26 19:00:20 +0300

    Update Home.jsx to add scrollToTop button or related UI changes

--- kukanApi/client/src/pages/Home/Home.jsx
+++ kukanApi/client/src/pages/Home/Home.jsx
@@ -2,20 +2,11 @@ import React from "react";
 import { sections, resources, footer } from "../../data/HomeData";
 import Section from "../../components/section/Section";
 import Resource from "../../components/Resource/Resource";
-import backToTopIcon from "../../assets/Agu_Display/backToTop.png";
+import BackToHome from "../../components/BackToHome";
 
 const Home = () => (
   <div className="bg-white">
-    {/* Back to Top Button */}
-    <div className="relative group">
-      <div className="backToTop fixed cursor-pointer right-4 bottom-4 w-12 h-12 rounded-full ring-1">
-        <img src={backToTopIcon} alt="Back to top" />
-      </div>
-      <div className="fixed hidden bottom-14 right-7 capitalize group-hover:block bg-gray-700 text-white text-sm rounded px-2 py-1">
-        back to top!
-      </div>
-    </div>
-
+    <BackToHome />
     {/* Dynamic Sections */}
     {sections.map((section, idx) => (
       <Section key={idx} {...section} reverse={idx % 2 === 1} />
@@ -38,8 +29,7 @@ const Home = () => (
         </div>
       </div>
     </section>
-    
-    
+
     {/* Footer Section */}
     <section className="xl:mt-44 md:mt-0 page text-center bg-gradient-to-r from-teal-50 to-blue-50 grid place-content-center w-full h-[40rem] md:h-screen pt-20 px-5">
       <div className="grid place-content-center">
